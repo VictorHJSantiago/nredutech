ac096631c41267be660896199eae30d1ad440190
kalivictor <kalivictor@kalivictor.kalivictor>
2025-08-03 16:10:50 -0300

    Import de todos os arquivos .css para o app.css e para o guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,8 +4,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>@yield('title') - NREduTech</title>
-
-  @vite(['resources/css/app.css', 'resources/css/login.css'])
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
   <div class="container">
