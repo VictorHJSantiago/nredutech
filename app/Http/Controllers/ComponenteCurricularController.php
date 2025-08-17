790cce84b537bf84b2ce0b4bb39253666188c65c
kalivictor <kalivictor@kalivictor.kalivictor>
2025-08-16 21:01:56 -0300

    feat(controllers): adiciona 10 arquivos controllers app/Http/Controllers

---
+++ app/Http/Controllers/ComponenteCurricularController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ComponenteCurricular;
+use Illuminate\Http\Request;
+
+class ComponenteCurricularController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ComponenteCurricular $componenteCurricular)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ComponenteCurricular $componenteCurricular)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ComponenteCurricular $componenteCurricular)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ComponenteCurricular $componenteCurricular)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/UsuarioPreferenciaController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UsuarioPreferencia;
+use Illuminate\Http\Request;
+
+class UsuarioPreferenciaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(UsuarioPreferencia $usuarioPreferencia)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(UsuarioPreferencia $usuarioPreferencia)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, UsuarioPreferencia $usuarioPreferencia)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(UsuarioPreferencia $usuarioPreferencia)
+    {
+        //
+    }
+}
